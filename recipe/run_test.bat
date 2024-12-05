7701717207158d5621a50792b1d0470c5e6323a5
Charles Bousseau <cbousseau@anaconda.com>
2024-12-05 16:01:39 -0500

    skip test_source_distribution

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,4 +8,4 @@ popd
 
 %PYTHON% -m pip check
 
-py.test -k "not (test_fortran_compiler or test_issue352_isolated_environment_support or test_cmake_install_dir_keyword or test_generator_selection or pep518 or test_configure_with_cmake_args or test_make_with_install_target)" -v
+py.test -k "not (test_source_distribution or test_fortran_compiler or test_issue352_isolated_environment_support or test_cmake_install_dir_keyword or test_generator_selection or pep518 or test_configure_with_cmake_args or test_make_with_install_target)" -v
