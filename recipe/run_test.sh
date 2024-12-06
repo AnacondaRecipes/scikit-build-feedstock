130eef170ba83a3522f3939e12d7307552945b03
Charles Bousseau <cbousseau@anaconda.com>
2024-12-06 18:01:38 -0500

    adjust tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,3 @@
 $PYTHON -m pip check
 
-py.test -k "not (test_source_distribution or test_fortran_compiler or test_issue352_isolated_environment_support or test_cmake_install_dir_keyword or test_generator_selection or pep518 or test_configure_with_cmake_args or test_cmake_args_keyword_osx_default)" -v
+pytest -m "not fortran and not isolated" -k "not test_issue352_isolated_environment_support and not test_configure_with_cmake_args and not test_generator_selection and not test_cmake_args_keyword_osx_default" -v
